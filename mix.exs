5602b805064ed9454686f462b8cbe8f88a5b46ea
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-12-13 14:19:19 +0100

    Bump version number.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Rational.Mixfile do
 
   def project do
     [app: :ratio,
-     version: "1.1.1",
+     version: "1.2.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
