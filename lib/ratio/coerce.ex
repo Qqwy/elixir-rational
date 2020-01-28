7943638be004932f9c2dc7312fcfa150c8fe7cb4
Kip Cole <kipcole9@gmail.com>
2020-01-28 18:48:08 +0800

    Reorder coercion

--- lib/ratio/coerce.ex
+++ lib/ratio/coerce.ex
@@ -1,11 +1,5 @@
 require Coerce
 
-Coerce.defcoercion(Ratio, Decimal) do
-  def coerce(ratio, decimal) do
-    {ratio, Ratio.DecimalConversion.decimal_to_rational(decimal)}
-  end
-end
-
 Coerce.defcoercion(Ratio, Integer) do
   def coerce(ratio, integer) do
     {ratio, Ratio.new(integer)}
@@ -15,5 +9,11 @@ end
 Coerce.defcoercion(Ratio, Float) do
   def coerce(ratio, float) do
     {ratio, Ratio.new(float)}
+  end
+end
+
+Coerce.defcoercion(Ratio, Decimal) do
+  def coerce(ratio, decimal) do
+    {ratio, Ratio.DecimalConversion.decimal_to_rational(decimal)}
   end
 end
