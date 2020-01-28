da3c847a31e917353f96f1a3cd63529e70d30808
Kip Cole <kipcole9@gmail.com>
2020-01-28 18:46:40 +0800

    Update coercion definitions

--- lib/ratio/coerce.ex
+++ lib/ratio/coerce.ex
@@ -5,3 +5,15 @@ Coerce.defcoercion(Ratio, Decimal) do
     {ratio, Ratio.DecimalConversion.decimal_to_rational(decimal)}
   end
 end
+
+Coerce.defcoercion(Ratio, Integer) do
+  def coerce(ratio, integer) do
+    {ratio, Ratio.new(integer)}
+  end
+end
+
+Coerce.defcoercion(Ratio, Float) do
+  def coerce(ratio, float) do
+    {ratio, Ratio.new(float)}
+  end
+end
