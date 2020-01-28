e61e007b17a6b88d7e5fd7c387dc85394839047a
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2020-01-28 09:19:39 +0100

    Whitespace

--- lib/ratio/coerce.ex
+++ lib/ratio/coerce.ex
@@ -1,12 +1,10 @@
 require Coerce
 
-
 Coerce.defcoercion(Ratio, Integer) do
   def coerce(ratio, integer) do
     {ratio, Ratio.new(integer)}
   end
 end
-
 
 Coerce.defcoercion(Ratio, Float) do
   def coerce(ratio, float) do
