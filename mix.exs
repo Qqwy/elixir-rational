c3b105ffee7ea63d026224e1e98093e4248760f8
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2020-03-17 20:37:11 +0100

    bump version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rational.Mixfile do
   def project do
     [
       app: :ratio,
-      version: "2.4.0",
+      version: "2.4.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
