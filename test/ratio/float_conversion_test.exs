f9d6a389e9037001a71ea2f8bdb6235d4b67ec7b
marcinwasowicz <marcin.wasowicz@swmansion.com>
2021-10-19 12:14:57 +0200

    Update flot to rational conversion test accordingly

--- test/ratio/float_conversion_test.exs
+++ test/ratio/float_conversion_test.exs
@@ -4,7 +4,7 @@ defmodule Ratio.FloatConversionTest do
   # use Ratio coerces negative floats to Ratios, so the below test needs to be run outside the Ratio.FloatConversion
   # module.
   test "float conversion for negative numbers" do
-    assert %Ratio{numerator: -11, denominator: 10} ==
-             Ratio.FloatConversion.float_to_rational(-1.1, 3)
+    assert %Ratio{numerator: -2476979795053773, denominator: 2251799813685248} ==
+             Ratio.FloatConversion.float_to_rational(-1.1)
   end
 end
